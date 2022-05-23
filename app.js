55569e3397c531fe1fb88a621fbc8231951654d5
JaysonKidoCode <99633158+JaysonKidoCode@users.noreply.github.com>
2022-05-23 17:11:55 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const navbarBtn = document.querySelector('.navbar_btn')
+const navbarLinks = document.querySelector('.navbar_links')
+
+navbarBtn.addEventListener('click',function(){
+    let value = navbarLinks.classList.contains('navbar_collapase')
+    
+    if(value){
+        navbarLinks.classList.remove('navbar_collapase')
+        navbarBtn.classList.remove('change')
+    }
+    else{
+        navbarLinks.classList.add('navbar_collapase')
+        navbarBtn.classList.add('change')
+    }
+    })
